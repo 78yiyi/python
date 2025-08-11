6473894645a81e6f8a1439da6fa72d4091cf7a83
78yiyi <156951560+78yiyi@users.noreply.github.com>
2025-08-11 19:05:38 +0200

    resub

---
+++ consitionals.py
@@ -0,0 +1,21 @@
+age=int(input("Give me your age, please: "))
+print(age>18)
+if age:
+    print(f"Tu edad es {age}")
+else:
+    print("No has escrito nada")
+
+'''if age>18:
+    print("You can pass")
+else:
+    print("Stop")'''
+
+if age>18:
+    print("Eres menor de edad")
+elif age<50:
+    print("Adelante")
+else:
+    print("Se te pasó el arroz")
+    print("No sé cómo se estará oyendo esto ahora")
+    print("Vamos a comprobarlo")
+
---
+++ ejercicios1.py
@@ -0,0 +1,24 @@
+#Ask the user for their name and age, assign theses values to two variables, and then print them.
+
+name=input("Please, type your name: ")
+age=input("Please, type your age: ")
+print("Your name is "+name+" and your age is "+age)
+
+#Investigate what happens when you try to assign a value to a variable that you’ve already defined. Try printing the variable before and after you reuse the name.
+
+""" a=2
+print(a)
+a=5
+print(a) """
+
+#Below you’ll find some code with a number of errors. Try to go through the program line by line and fix the issues in the code. I’d encourage you to try 
+# running the program while you’re working on it, as reading the error messages is great practice for debugging your own programs.
+
+hourly_wage = input("Please enter your hourly wage: ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+hours_worked=input("Hours worked: ")
+print(hours_worked)
+
+hours_worked = input("How many hours did you work this week? ")
