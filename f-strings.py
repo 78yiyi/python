c15c8e800b6a663042df44e93c52cd1e0e31acbb
78yiyi <lparra@lasallegrinon.es>
2026-04-12 17:38:41 +0200

    asd

--- f-strings.py
+++ f-strings.py
@@ -1,3 +1,5 @@
-num=10;
+num=11.5;
 name=input("Dime tu nombre: ");
 print(f"El nº de veces que aparece la 'a' es {name.count('a')}")
+print(f"Ahora voy a imprimirlo con dos decimales: {num:.2f}")
+print(f"Ahora voy a usar la función round {round(num)}")
